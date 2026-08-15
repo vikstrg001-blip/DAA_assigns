167670f76c5af6c146a6d9cdbb6057be5a06ccad
vikstrg001-blip <vik.strg001@gmail.com>
2026-08-16 02:14:09 +0530

    Add files via upload

---
+++ clgp1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+int sum(int a, int r, int n)
+{
+    int summ = 0;
+    return (a * (pow(r, n) - 1)) / (r - 1);
+}
+int main()
+{
+    int a, r, n;
+    printf("Enter  first term: ");
+    scanf("%d", &a);
+    printf("Enter common ratio: ");
+    scanf("%d", &r);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Sum : %d.\n", sum(a, r, n));
+    return 0;
+}
